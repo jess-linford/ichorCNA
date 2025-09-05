deb8cf6d8443bf3a3a981dffa719dc1dd686a26e
Jessica Linford <jess.linford@gmail.com>
2025-09-05 11:19:23 -0600

    Modify create_sample_yaml.py to sort bam files alphabetically

--- scripts/snakemake/config/create_sample_yaml.py
+++ scripts/snakemake/config/create_sample_yaml.py
@@ -3,7 +3,6 @@
 
 # In[1]:
 
-
 import glob
 import pathlib
 import os
@@ -14,15 +13,16 @@ bamfolder = sys.argv[1]
 
 # In[2]:
 
-
 bam_files = []
 for filepath in pathlib.Path(bamfolder).glob('**/*.bam'):
     bam_files.append(str(filepath.absolute()))
 bam_files
 
+# Sort bam files alphabetically by filename
+bam_files.sort(key=lambda x: os.path.basename(x))
+
 
 # In[3]:
-
 
 f = open("samples.yaml", "w")
 f.write('samples:\n')
